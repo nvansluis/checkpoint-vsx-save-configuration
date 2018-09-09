2b1067a7dfbe80dae3b7c702fda751ce397babcf
Niels van Sluis <niels@van-sluis.nl>
2018-09-09 15:30:34 +0200

    added backup of important configuration files

--- backup_configuration_vsx.sh
+++ backup_configuration_vsx.sh
@@ -3,16 +3,57 @@
 # backup_configuration_vsx.sh - niels@van-sluis.nl
 #
 # v0.1 - 2018-09-05
+# v0.2 - 2018-09-09
 
 TEMPDIR=/tmp/backup_configuration_vsx-$$
 DATE=`date +%Y-%m-%d`
 IDENTIFIER=backup_configuration_vsx-$HOSTNAME-$DATE
-FILENAME=$IDENTIFIER.tar
+FILENAME=$IDENTIFIER.tar.gz
 OUTPUTDIR=/tmp
+
+# get CP and VSX environment
+. /etc/profile.d/CP.sh
+. /etc/profile.d/vsenv.sh
 
 # get configured virtual systems
 #LISTVS=(`clish -c 'show virtual-system all' | tail -n +3 | awk {'print $1'} | tr '\r\n' ' '`)
 LISTVS=(`vrf list vrfs | tr '\r\n' ' '`)
+
+
+function save_files() {
+    # change to context of virtual system
+    vsenv $vs > /dev/null
+
+    # define files to backup
+    FILES=($FWDIR/boot/modules/fwkern.conf
+           $FWDIR/boot/modules/vpnkern.conf
+           $PPKDIR/boot/modules/simkern.conf
+           $PPKDIR/boot/modules/sim_aff.conf
+           $FWDIR/conf/fwaffinity.conf
+           $FWDIR/conf/fwauthd.conf
+           $FWDIR/conf/local.arp
+           $FWDIR/conf/discntd.if
+           $FWDIR/conf/cpha_bond_ls_config.conf
+           $FWDIR/conf/resctrl
+           $FWDIR/conf/vsaffinity_exception.conf
+           $FWDIR/database/qos_policy.C
+           $FWDIR/conf/sdconf.rec
+           $FWDIR/conf/sdopts.rec
+           $FWDIR/conf/sdstatus.12
+           $FWDIR/conf/securid
+           /var/ace/sdconf.rec
+           /var/ace/sdopts.rec
+           /var/ace/sdstatus.12
+           /var/ace/securid)
+
+    for file in "${FILES[@]}"
+    do
+        DIR=$(dirname "${file}")
+        DSTDIR="${TEMPDIR}/${IDENTIFIER}/${vsname}${DIR}"
+        [ -f $file ] && mkdir -p ${DSTDIR}
+        [ -f $file ] && cp ${file} ${DSTDIR}
+    done
+}
 
 # create temporary directory
 mkdir -p $TEMPDIR/$IDENTIFIER
@@ -26,6 +67,9 @@ do
 
   # save config
   clish -f $TEMPDIR/clish-input.txt > $TEMPDIR/$IDENTIFIER/vs_$vs-config.txt
+
+  # save important files
+  save_files $vs
 done
 
 # change workdir
@@ -35,12 +79,5 @@ cd $TEMPDIR
 tar czf $OUTPUTDIR/$FILENAME $IDENTIFIER/
 echo "Saved configs: $OUTPUTDIR/$FILENAME"
 
-# remove configs
-rm $TEMPDIR/$IDENTIFIER/vs_*-config.txt
-
-# remove clish input file
-rm $TEMPDIR/clish-input.txt
-
 # remove temporary directory
-rmdir $TEMPDIR/$IDENTIFIER
-rmdir $TEMPDIR
+rm -rf $TEMPDIR
